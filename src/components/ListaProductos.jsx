cdf9b710dc6e5e2fbe1fcb56686af329cb5d951e
Marco <mespinozapoma@gmail.com>
2025-10-09 15:08:43 -0300

    Primer commit: estructura base del proyecto

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,19 +1,17 @@
+
 useEffect(() => {
   const obtenerProductos = async () => {
-    const url = "https://raw.githubusercontent.com/Quincenero/25235-marco-espinoza/main/src/data/productos.json";
+    const url = "https://raw.githubusercontent.com/Quincenero/25235-marco-espinoza/refs/heads/main/src/data/productos.json";
+    
     try {
       const response = await fetch(url);
+      console.log("Response status:", response.status);
       if (!response.ok) throw new Error("Error al obtener productos");
       const data = await response.json();
-
-      // Adaptar campos del JSON a los que usa el JSX
-      const productosFormateados = data.map((prod) => ({
-        ...prod,
-        imagen: prod.img,
-        valor: prod.precio,
-      }));
-
-      setProductos(productosFormateados);
+      
+      // Usar los datos directamente sin mapear
+      setProductos(data);
+      
     } catch (error) {
       console.error("Error al cargar productos:", error);
       setProductos([]);
