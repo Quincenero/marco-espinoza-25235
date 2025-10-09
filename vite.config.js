225e68f767650e4800017c8569274fc252ca34fe
Marco <mespinozapoma@gmail.com>
2025-10-09 20:57:06 -0300

    configurcion de vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/verduleria-organica/',
+  base: 'marco-espinoza-25235/',
   plugins: [react()],
 })
