53b19c15b8e031d82de4760e28cbb0a78be1862b
Marco <mespinozapoma@gmail.com>
2025-10-10 10:31:13 -0300

    fix: solucion de dist/

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 export const CarritoContext = createContext();
 
