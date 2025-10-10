432a3711968e7970af59d588933d30af275ef041
Marco <mespinozapoma@gmail.com>
2025-10-10 10:45:49 -0300

    deply para github

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/marco-espinoza-25235/', //nombre del repo
+  build: {
+    outDir: 'dist'
+  }
 })
