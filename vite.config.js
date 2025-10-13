66ace9e3fc3fcec2bb6b8b3cab73f27185a3c5ea
Marco <mespinozapoma@gmail.com>
2025-10-13 09:48:01 -0300

    cambio en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/marco-espinoza-25235/',
   build: {
     outDir: 'docs',  // Cambia dist por docs
     emptyOutDir: true
