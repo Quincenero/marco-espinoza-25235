e4ce86e5985728472a26edfdeb7b5ccadb5a2d7d
Marco <mespinozapoma@gmail.com>
2025-10-14 22:42:26 -0300

    corrección de la configuración de despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detectar destino de despliegue
+const deployTarget = process.env.DEPLOY_TARGET
+
+const isGitHubPages = deployTarget === 'gh-pages'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/marco-espinoza-25235/',
+  base: isGitHubPages ? '/marco-espinoza-25235/' : '/',
   build: {
-    outDir: 'docs',  // Cambia dist por docs
+    outDir: isGitHubPages ? 'docs' : 'dist',
     emptyOutDir: true
   }
 })
